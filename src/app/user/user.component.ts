8a1895bcf8f1d2693b5964b5f55d971a201a726d
Marlon Costa <mrlnrnn@gmail.com>
2025-01-23 18:01:57 -0300

    Tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+      this.select.emit(this.id);
+  }
 }
