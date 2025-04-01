edfa1a70f769a59f976d60ee542dda714f49a413
Marlon Costa <mrlnrnn@gmail.com>
2025-04-01 19:48:32 -0300

    Adding tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,22 +1,22 @@
-import { Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
-  selector: 'app-user',
+  selector: 'app-user', 
   imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-      this.select.emit(this.id);
+      this.select.emit(this.user.id);
   }
 }
